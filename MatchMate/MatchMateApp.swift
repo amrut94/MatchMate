bc0ccd77977611354220508db6a6917e2826f3db
Amrut Waghmare <amrut4u2014@gmail.com>
2024-07-24 12:33:24 +0530

    Project Setup

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by AMRUT WAGHMARE on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
